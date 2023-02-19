12467f488892733247ff1b541ee2f94c22434518
Anthony Bonta <zthun.coding@gmail.com>
2023-02-19 11:57:52 -0600

    style: update theme

--- packages/pokedex-web/src/pokedex-app/pokedex-app.tsx
+++ packages/pokedex-web/src/pokedex-app/pokedex-app.tsx
@@ -1,7 +1,16 @@
+import { GlobalStyles, ThemeProvider } from '@mui/material';
 import React from 'react';
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { ZPokedexDetailsPage } from '../pokedex-details/pokedex-details-page';
 import { ZPokedexListPage } from '../pokedex-list/pokedex-list-page';
+import { usePokedexTheme } from '../theme/make-styles';
+
+const globalStyles = {
+  body: {
+    backgroundColor: 'whitesmoke',
+    padding: '3rem'
+  }
+};
 
 /**
  * Represents the root entry point into the application.
@@ -10,15 +19,20 @@ import { ZPokedexListPage } from '../pokedex-list/pokedex-list-page';
  *        The jsx to render the pokedex web application.
  */
 export function ZPokedexApp() {
+  const theme = usePokedexTheme();
+
   return (
-    <main className='ZPokedexApp-root'>
-      <HashRouter>
-        <Routes>
-          <Route path='/pokemon/:idOrName' element={<ZPokedexDetailsPage />} />
-          <Route path='/pokemon' element={<ZPokedexListPage />} />
-          <Route path='*' element={<Navigate to={'/pokemon'} />} />
-        </Routes>
-      </HashRouter>
+    <main className='ZPokedexApp-root' style={{ display: 'flex', justifyContent: 'center' }}>
+      <GlobalStyles styles={globalStyles} />
+      <ThemeProvider theme={theme}>
+        <HashRouter>
+          <Routes>
+            <Route path='/pokemon/:idOrName' element={<ZPokedexDetailsPage />} />
+            <Route path='/pokemon' element={<ZPokedexListPage />} />
+            <Route path='*' element={<Navigate to={'/pokemon'} />} />
+          </Routes>
+        </HashRouter>
+      </ThemeProvider>
     </main>
   );
 }
