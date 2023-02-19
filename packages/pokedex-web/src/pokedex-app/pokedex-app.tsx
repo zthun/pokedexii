1d60bfd04433bddbb33be4059159bd5cef1c9994
Anthony Bonta <zthun.coding@gmail.com>
2023-02-19 10:08:06 -0600

    chore: routing

--- packages/pokedex-web/src/pokedex-app/pokedex-app.tsx
+++ packages/pokedex-web/src/pokedex-app/pokedex-app.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { ZPokedexDetailsPage } from '../pokedex-details/pokedex-details-page';
+import { ZPokedexListPage } from '../pokedex-list/pokedex-list-page';
 
 /**
  * Represents the root entry point into the application.
@@ -7,5 +10,15 @@ import React from 'react';
  *        The jsx to render the pokedex web application.
  */
 export function ZPokedexApp() {
-  return <div className='ZPokedexApp-root'>Pokedex!</div>;
+  return (
+    <main className='ZPokedexApp-root'>
+      <HashRouter>
+        <Routes>
+          <Route path='/pokemon/:idOrName' element={<ZPokedexDetailsPage />} />
+          <Route path='/pokemon' element={<ZPokedexListPage />} />
+          <Route path='*' element={<Navigate to={'/pokemon'} />} />
+        </Routes>
+      </HashRouter>
+    </main>
+  );
 }
