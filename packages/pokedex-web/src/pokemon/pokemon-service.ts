9c155a313c86853a97f825cde198d277d251da1b
Anthony Bonta <zthun.coding@gmail.com>
2023-02-19 15:00:23 -0600

    refactor: cleanup

--- packages/pokedex-web/src/pokemon/pokemon-service.ts
+++ packages/pokedex-web/src/pokemon/pokemon-service.ts
@@ -3,15 +3,9 @@ import { ZHttpService } from '@zthun/works.http';
 import { createContext, useContext } from 'react';
 import { useAsyncState } from '../state/use-async-state';
 
-export function createDefaultPokemonService(): IZPokemonService {
-  return new ZPokemonServiceHttp(new ZHttpService());
-}
-
+export const createDefaultPokemonService: () => IZPokemonService = () => new ZPokemonServiceHttp(new ZHttpService());
 export const ZPokemonServiceContext = createContext(createDefaultPokemonService());
-
-export function usePokemonService() {
-  return useContext(ZPokemonServiceContext);
-}
+export const usePokemonService = () => useContext(ZPokemonServiceContext);
 
 export function usePokemonPage(request: IZDataRequest) {
   const service = usePokemonService();
