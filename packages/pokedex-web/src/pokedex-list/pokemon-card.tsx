745226046e08cd0c86d45e98fce7bc926aa4e64c
Anthony Bonta <zthun.coding@gmail.com>
2023-02-19 17:04:01 -0600

    feat: pokemon cards now have ellipse headers

--- packages/pokedex-web/src/pokedex-list/pokemon-card.tsx
+++ packages/pokedex-web/src/pokedex-list/pokemon-card.tsx
@@ -11,27 +11,47 @@ export interface IZPokemonCard {
   onClick?: MouseEventHandler<HTMLDivElement>;
 }
 
-const usePokemonCardStyles = makeStyles()(() => ({
+const usePokemonCardStyles = makeStyles()((theme) => ({
   card: {
     cursor: 'pointer',
-    width: '10rem'
+    width: '15rem',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  header: {
+    '.MuiCardHeader-content': {
+      width: '100%'
+    },
+
+    '.MuiCardHeader-title': {
+      overflow: 'hidden',
+      whiteSpace: 'nowrap',
+      textOverflow: 'ellipsis'
+    }
+  },
+  sprite: {
+    width: '6rem',
+    alignSelf: 'center',
+    padding: theme.spacing()
   }
 }));
 
 export function ZPokemonCard(props: IZPokemonCard) {
   const { pokemon, onClick } = props;
-  const styles = usePokemonCardStyles();
+  const title = startCase(pokemon.name);
+  const { classes } = usePokemonCardStyles();
 
   return (
     <Card
-      className={cssClass('ZPokemonCard-root', styles.classes.card)}
+      className={cssClass('ZPokemonCard-root', classes.card)}
       data-name={pokemon.name}
       data-id={pokemon.id}
       onClick={onClick}
+      title={title}
     >
-      <CardHeader className='ZPokemonCard-header' title={startCase(pokemon.name)} subheader={pokemon.id}></CardHeader>
+      <CardHeader className={cssClass('ZPokemonCard-header', classes.header)} title={title} subheader={pokemon.id} />
       <CardMedia
-        className='ZPokemonCard-sprite'
+        className={cssClass('ZPokemonCard-sprite', classes.sprite)}
         component='img'
         src={pokemon.sprites?.front_default}
         alt={pokemon.name}
