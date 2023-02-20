e795dbef64a88e2d3676bdecaa088ed9b6fbe8ea
Anthony Bonta <zthun.coding@gmail.com>
2023-02-19 20:50:58 -0600

    refactor: simplified implementation

--- packages/pokedex/src/source/data-source-async.spec.ts
+++ packages/pokedex/src/source/data-source-async.spec.ts
@@ -1,6 +1,6 @@
 import { range } from 'lodash';
 import { beforeEach, describe, expect, it } from 'vitest';
-import { IZDataRequest, ZDataRequestBuilder } from './data-request';
+import { ZDataRequestBuilder } from './data-request';
 import { ZDataSourceAsync } from './data-source-async';
 
 describe('ZDataSourceAsync', () => {
@@ -15,49 +15,28 @@ describe('ZDataSourceAsync', () => {
   });
 
   describe('Count', () => {
-    it('should return the total count of the data unpaginated', async () => {
+    it('should return the total count of the data', async () => {
       // Arrange
       const target = createTestTarget();
       const expected = (await data).length;
+      const request = new ZDataRequestBuilder().build();
       // Act
-      const result = await target.count();
+      const result = await target.count(request);
       // Assert
       expect(result).toEqual(expected);
     });
   });
 
   describe('Retrieve', () => {
-    async function shouldResultInPage(expected: number[], request: IZDataRequest) {
+    it('should return the requested page', async () => {
+      // Arrange
       const target = createTestTarget();
+      const request = new ZDataRequestBuilder().size(100).build();
+      const expected = range(1, 101);
       // Act.
       const result = await target.retrieve(request);
       // Assert.
       expect(result).toEqual(expected);
-    }
-
-    it('should return an empty page if the page size goes beyond the final page', async () => {
-      const request = new ZDataRequestBuilder().page(1000).size(1000).build();
-      await shouldResultInPage([], request);
-    });
-
-    it('should return the entire data set if the size is infinite and the page is equal to 1', async () => {
-      const request = new ZDataRequestBuilder().build();
-      await shouldResultInPage(await data, request);
-    });
-
-    it('should return an empty page if size is infinite, but page is greater than 1', async () => {
-      const request = new ZDataRequestBuilder().page(2).build();
-      await shouldResultInPage([], request);
-    });
-
-    it('should return the requested page', async () => {
-      const request = new ZDataRequestBuilder().page(2).size(100).build();
-      await shouldResultInPage(range(101, 201), request);
-    });
-
-    it('should return the last page remaining data', async () => {
-      const request = new ZDataRequestBuilder().page(11).size(100).build();
-      await shouldResultInPage(range(1001, 1101), request);
     });
   });
 });
--- packages/pokedex/src/source/data-source-async.ts
+++ packages/pokedex/src/source/data-source-async.ts
@@ -1,24 +1,22 @@
+import { IZDataMatch } from './data-match';
 import { IZDataRequest } from './data-request';
-import { paginate } from './data-results';
 import { IZDataSource } from './data-source';
+import { ZDataSourceStatic } from './data-source-static';
 
 /**
  * Represents a data source that retrieves a full list of
  * data asynchronously
  */
 export class ZDataSourceAsync<T> implements IZDataSource<T> {
-  public constructor(private _async: Promise<T[]>) {}
+  public constructor(private _async: Promise<T[]>, private _search?: IZDataMatch<T, string>) {}
 
-  public async count(): Promise<number> {
-    // TODO: filtering.
+  public async count(request: IZDataRequest): Promise<number> {
     const data = await this._async;
-    return data.length;
+    return new ZDataSourceStatic(data, this._search).count(request);
   }
 
   public async retrieve(request: IZDataRequest): Promise<T[]> {
-    // TODO: Sorting, Filtering
-    const { page = 1, size = Infinity } = request;
     const data = await this._async;
-    return paginate(data, page, size);
+    return new ZDataSourceStatic(data, this._search).retrieve(request);
   }
 }
