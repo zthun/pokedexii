defe5e118124f2055c9ef5c0b7f6e0748f2ca7c0
Anthony Bonta <zthun.coding@gmail.com>
2023-07-08 17:24:27 -0500

    refactor: added a usePokemon helper hook

--- packages/pokedex-web/src/pokemon/pokemon-service.ts
+++ packages/pokedex-web/src/pokemon/pokemon-service.ts
@@ -1,3 +1,4 @@
+import { useAsyncState } from '@zthun/helpful-react';
 import { createPokemonService } from '@zthun/pokedex';
 import { createContext, useContext } from 'react';
 
@@ -5,4 +6,9 @@ export const ZPokemonServiceContext = createContext(createPokemonService());
 
 export function usePokemonService() {
   return useContext(ZPokemonServiceContext);
+}
+
+export function usePokemon(name: string | null | undefined) {
+  const service = usePokemonService();
+  return useAsyncState(() => (name ? service.get(name) : Promise.reject('Pokemon not found')), [name]);
 }
