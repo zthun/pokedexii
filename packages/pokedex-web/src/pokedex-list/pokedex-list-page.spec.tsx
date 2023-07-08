47405e9d65b6b874ad346a82b9b16ad2d0386616
Anthony Bonta <zthun.coding@gmail.com>
2023-07-07 22:32:14 -0500

    test: added list page tests

--- packages/pokedex-web/src/pokedex-list/pokedex-list-page.spec.tsx
+++ packages/pokedex-web/src/pokedex-list/pokedex-list-page.spec.tsx
@@ -1,6 +1,7 @@
 import { ZCircusBy } from '@zthun/cirque';
 import { ZCircusSetupRenderer } from '@zthun/cirque-du-react';
 import { ZTestRouter } from '@zthun/fashion-boutique';
+import { ZDataSearchFields, ZDataSourceStatic, ZDataSourceStaticOptionsBuilder } from '@zthun/helpful-query';
 import { IZPokemon, IZPokemonService, ZPokemonBuilder } from '@zthun/pokedex';
 import { MemoryHistory, createMemoryHistory } from 'history';
 import React from 'react';
@@ -41,16 +42,59 @@ describe('ZPokedexListPage', () => {
     pikachu = new ZPokemonBuilder().pikachu().build();
     pokemon = [bulbasaur, charmander, squirtle, pikachu];
 
+    const options = new ZDataSourceStaticOptionsBuilder<IZPokemon>().search(new ZDataSearchFields(['name'])).build();
+    const source = new ZDataSourceStatic(pokemon, options);
+
     pokemonService = mock<IZPokemonService>();
-    pokemonService.retrieve.mockResolvedValue(pokemon);
-    pokemonService.count.mockResolvedValue(pokemon.length);
+    pokemonService.retrieve.mockImplementation((r) => source.retrieve(r));
+    pokemonService.count.mockImplementation((r) => source.count(r));
   });
 
-  it('should render the component', async () => {
+  it('should render the list of pokemon on the first page', async () => {
     // Arrange.
-    // Act.
     const target = await createTestTarget();
+    // Act.
+    const actual = await target.cards();
     // Assert.
-    expect(target).toBeTruthy();
+    expect(actual.length).toEqual(pokemon.length);
+  });
+
+  it('should render named cards by the pokemon page list', async () => {
+    // Arrange.
+    const target = await createTestTarget();
+    const expected = pokemon.map((p) => p.name);
+    expected.sort();
+    // Act.
+    const cards = await target.cards();
+    const _pokemon = await Promise.all(cards.map((c) => c.who()));
+    const actual = _pokemon.map((p) => p.name);
+    actual.sort();
+    // Assert.
+    expect(actual).toEqual(expected);
+  });
+
+  it('should render the list of searched pokemon', async () => {
+    // Arrange.
+    const target = await createTestTarget();
+    const grid = await target.grid();
+    const finder = await grid.search();
+    // Act.
+    await finder.keyboard(pikachu.name);
+    const cards = await target.cards();
+    const actual = await target.card(pikachu.name);
+    // Assert.
+    expect(cards.length).toEqual(1);
+    expect(actual).toBeTruthy();
+  });
+
+  it('should navigate to the details page of a pokemon when the card is clicked', async () => {
+    // Arrange.
+    const target = await createTestTarget();
+    const card = await target.card(charmander.name);
+    // Act.
+    await card?.click();
+    const actual = history.location.pathname;
+    // Assert.
+    expect(actual).toEqual(`/pokemon/${charmander.name}`);
   });
 });
