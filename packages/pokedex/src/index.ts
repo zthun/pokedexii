72d6ab4bb580382f410012be195f68967e06576e
Anthony Bonta <zthun.coding@gmail.com>
2023-07-09 17:20:23 -0500

    chore: cleanup

--- packages/pokedex/src/index.ts
+++ packages/pokedex/src/index.ts
@@ -1,9 +1,8 @@
 export * from './pokedex-resource/pokedex-named-resource';
 export * from './pokedex-resource/pokedex-resource-service';
-export * from './pokemon/pokemon-service';
 
 export * from './pokemon/pokemon';
-export * from './pokemon/pokemon-stat';
+export * from './pokemon/pokemon-service';
 
 export * from './type/type';
 export * from './type/type-service';
