24a9b8eee180e97ecb3279268f067d7c02b3a05d
Anthony Bonta <zthun.coding@gmail.com>
2023-07-09 00:05:09 -0500

    refactor: move to own directory

--- packages/pokedex/src/index.ts
+++ packages/pokedex/src/index.ts
@@ -1,4 +1,4 @@
+export * from './pokemon-type/pokemon-type';
 export * from './pokemon/pokemon';
 export * from './pokemon/pokemon-service';
 export * from './pokemon/pokemon-stat';
-export * from './pokemon/pokemon-type';
