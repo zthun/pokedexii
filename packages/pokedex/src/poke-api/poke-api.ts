e764a4b4e5b2fdf2694d8c413ad37d971ef35851
Anthony Bonta <zthun.coding@gmail.com>
2023-07-09 23:30:28 -0500

    fix: api resource should now cache properly

--- packages/pokedex/src/poke-api/poke-api.ts
+++ packages/pokedex/src/poke-api/poke-api.ts
@@ -1,9 +1,10 @@
 /* istanbul ignore file -- @preserve */
 
+import { ZHttpRequestBuilder, ZHttpService } from '@zthun/webigail-http';
+import { ZUrlBuilder } from '@zthun/webigail-url';
+import localForage from 'localforage';
 import { IPokeApiPage } from './poke-api-page';
 import { IPokeApiPokemon } from './poke-api-pokemon';
-
-import { Pokedex } from 'pokeapi-js-wrapper';
 import { IPokeApiType } from './poke-api-type';
 
 export interface IPokeApi {
@@ -14,39 +15,58 @@ export interface IPokeApi {
 }
 
 export class ZPokeApi implements IPokeApi {
-  private _types = new Map<string, Promise<IPokeApiType>>();
-  private _pokemon = new Map<string, Promise<IPokeApiPokemon>>();
-
-  private _pokedex = new Pokedex();
+  private static readonly DATABASE = 'https://pokeapi.co/api/v2';
+  private _flightMap = new Map<string, Promise<any>>();
+  private _http = new ZHttpService();
 
   private static _instance = new ZPokeApi();
 
   public static instance() {
+    localForage.config({ name: ZPokeApi.DATABASE });
     return ZPokeApi._instance;
   }
 
   private constructor() {}
 
-  private _getFromMap<T>(name: string, map: Map<string, Promise<T>>, getFn: (name: string) => Promise<T>) {
-    if (!map.has(name)) {
-      map.set(name, getFn(name));
+  private _resourceUrl(resource: string, name?: string) {
+    const root = new ZUrlBuilder('https', 'pokeapi.co').append('api').append('v2').append(resource);
+    return name == null ? root.param('limit', '10000').build() : root.append(name).build();
+  }
+
+  private async _resource<T>(resource: string, name?: string): Promise<T> {
+    const url = this._resourceUrl(resource, name);
+    const request = new ZHttpRequestBuilder().url(url).get().timeout(10000).build();
+
+    const cached = await localForage.getItem<T>(url);
+
+    if (cached == null) {
+      if (!this._flightMap.has(url)) {
+        this._flightMap.set(url, this._http.request(request))!;
+      }
+
+      const response = await this._flightMap.get(url)!;
+      const { data } = response;
+      await localForage.setItem<T>(url, data);
+      this._flightMap.delete(url);
+      return data;
     }
-    return map.get(name)!;
+
+    return cached;
   }
 
   public pokemons(): Promise<IPokeApiPage> {
-    return this._pokedex.getPokemonsList();
+    return this._resource('pokemon');
   }
 
   public pokemon(name: string): Promise<IPokeApiPokemon> {
-    return this._getFromMap(name, this._pokemon, this._pokedex.getPokemonByName.bind(this._pokedex));
+    return this._resource('pokemon', name);
   }
 
   public types(): Promise<IPokeApiPage> {
-    return this._pokedex.getTypesList();
+    return this._resource('type');
   }
 
   public type(name: string): Promise<IPokeApiType> {
-    return this._getFromMap(name, this._types, this._pokedex.getTypeByName.bind(this._pokedex));
+    return this._resource('type', name);
   }
 }
