78de02443896353d9c81631f388f8beebf192bfa
Anthony Bonta <zthun.coding@gmail.com>
2023-07-10 20:28:06 -0500

    feat: ids can now be strings or numbers

--- packages/pokedex/src/pokedex-resource/pokedex-named-resource.ts
+++ packages/pokedex/src/pokedex-resource/pokedex-named-resource.ts
@@ -4,6 +4,6 @@
  * Every resource in the pokemon world has a name and this
  * serves as an id to the given resource.
  */
-export interface IZPokedexNamedResource<TName extends string = string> {
+export interface IZPokedexNamedResource<TName extends string | number = string> {
   name: TName;
 }
