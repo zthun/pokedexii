ddaef90dd56eee30a142793d6cf0779886c54be3
Anthony Bonta <zthun.coding@gmail.com>
2023-07-16 23:51:26 -0500

    fix: missing assignments for relative stats and beauty

--- packages/pokedex/src/evolution/evolution-service.ts
+++ packages/pokedex/src/evolution/evolution-service.ts
@@ -41,6 +41,7 @@ class ZEvolutionService implements Converter, Retriever {
         .turnUpsideDown(details.turn_upside_down);
 
       trigger = details.min_affection != null ? trigger.affection(details.min_affection) : trigger;
+      trigger = details.min_beauty != null ? trigger.beauty(details.min_beauty) : trigger;
       trigger = details.gender != null ? trigger.gender(genders[details.gender - 1]) : trigger;
       trigger = details.min_happiness != null ? trigger.happiness(details.min_happiness) : trigger;
       trigger = details.held_item != null ? trigger.held(details.held_item.name) : trigger;
@@ -48,11 +49,16 @@ class ZEvolutionService implements Converter, Retriever {
       trigger = details.min_level != null ? trigger.level(details.min_level) : trigger;
       trigger = details.location != null ? trigger.location(details.location.name) : trigger;
       trigger = details.known_move != null ? trigger.move(details.known_move.name) : trigger;
-      trigger = details.known_move_type != null ? trigger.move(details.known_move_type.name) : trigger;
+      trigger = details.known_move_type != null ? trigger.moveType(details.known_move_type.name) : trigger;
       trigger = details.party_species != null ? trigger.partySpecies(details.party_species.name) : trigger;
       trigger = details.party_type != null ? trigger.partyType(details.party_type.name) : trigger;
       trigger = details.time_of_day ? trigger.time(details.time_of_day) : trigger;
       trigger = details.trade_species ? trigger.trade(details.trade_species.name) : trigger;
+
+      trigger =
+        details.relative_physical_stats != null
+          ? trigger.stats(details.relative_physical_stats.map((s) => s.name))
+          : trigger;
 
       return trigger.build();
     };
