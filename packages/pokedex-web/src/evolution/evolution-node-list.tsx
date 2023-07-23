5a1517be61c88b98f72539805f63e6bd0ac51aee
Anthony Bonta <zthun.coding@gmail.com>
2023-07-22 20:58:18 -0500

    feat: render trigger information for evolutions

--- packages/pokedex-web/src/evolution/evolution-node-list.tsx
+++ packages/pokedex-web/src/evolution/evolution-node-list.tsx
@@ -1,8 +1,18 @@
-import { IZComponentName, ZCarousel, ZIconFontAwesome, createStyleHook } from '@zthun/fashion-boutique';
+import {
+  IZComponentName,
+  ZBox,
+  ZCaption,
+  ZCarousel,
+  ZIconFontAwesome,
+  ZLabel,
+  ZStack,
+  createStyleHook
+} from '@zthun/fashion-boutique';
 import { ZSizeFixed } from '@zthun/fashion-tailor';
 import { cssJoinDefined } from '@zthun/helpful-fn';
-import { IZEvolutionNode } from '@zthun/pokedex';
-import React, { useState } from 'react';
+import { IZEvolutionNode, IZEvolutionTrigger } from '@zthun/pokedex';
+import { startCase } from 'lodash';
+import React, { ReactNode, useState } from 'react';
 import { ZEvolutionNodeBubble } from './evolution-node-bubble';
 
 export interface IZEvolutionNodeList extends IZComponentName {
@@ -34,6 +44,70 @@ export function ZEvolutionNodeList(props: IZEvolutionNodeList) {
     />
   );
 
+  const renderTrigger = (trigger: IZEvolutionTrigger) => {
+    const requirements: ReactNode[] = [];
+
+    const addRequirement = (value: any, name: string) => {
+      if (value != null && value !== false) {
+        const text = `${name}: ${startCase(String(value))}`;
+        requirements.push(
+          <ZCaption key={name} compact>
+            {text}
+          </ZCaption>
+        );
+      }
+    };
+
+    requirements.push(<ZLabel key={trigger.name}>{startCase(trigger.name)}</ZLabel>);
+
+    addRequirement(trigger.affection, 'Affection');
+    addRequirement(trigger.beauty, 'Beauty');
+    addRequirement(trigger.gender, 'Gender');
+    addRequirement(trigger.happiness, 'Happiness');
+    addRequirement(trigger.held, 'Item');
+    addRequirement(trigger.item, 'Item');
+    addRequirement(trigger.level, 'Level');
+    addRequirement(trigger.location, 'Location');
+    addRequirement(trigger.move, 'Move');
+    addRequirement(trigger.moveType, 'Move Type');
+    addRequirement(trigger.partySpecies, 'Party Species');
+    addRequirement(trigger.partyType, 'Party Type');
+    addRequirement(trigger.rain, 'Rain');
+    addRequirement(trigger.stats, 'Stats');
+    addRequirement(trigger.time, 'Time');
+    addRequirement(trigger.trade, 'Trade');
+    addRequirement(trigger.turnUpsideDown, 'Turn Upside Down');
+
+    return (
+      <div className='ZEvolution-trigger' key={trigger.name}>
+        {requirements}
+      </div>
+    );
+  };
+
+  const renderTriggers = () => {
+    const { triggers } = nodes[index];
+
+    if (!triggers.length) {
+      return null;
+    }
+
+    return (
+      <ZStack className='ZEvolutionNodeList-triggers' alignItems='center' justifyContent='center'>
+        <ZBox margin={{ bottom: ZSizeFixed.ExtraSmall }}>
+          <ZIconFontAwesome
+            className={cssJoinDefined('ZEvolutionNodeList-next', classes.next)}
+            name='right-long'
+            width={ZSizeFixed.Small}
+          />
+        </ZBox>
+        <ZStack alignItems='start' gap={ZSizeFixed.ExtraSmall}>
+          {triggers.map(renderTrigger)}
+        </ZStack>
+      </ZStack>
+    );
+  };
+
   const renderNext = () => {
     const { next } = nodes[index];
 
@@ -43,11 +117,6 @@ export function ZEvolutionNodeList(props: IZEvolutionNodeList) {
 
     return (
       <>
-        <ZIconFontAwesome
-          className={cssJoinDefined('ZEvolutionNodeList-next', classes.next)}
-          name='right-long'
-          width={ZSizeFixed.Small}
-        />
         <ZEvolutionNodeList nodes={next} />
       </>
     );
@@ -55,6 +124,7 @@ export function ZEvolutionNodeList(props: IZEvolutionNodeList) {
 
   return (
     <>
+      {renderTriggers()}
       {renderNodeCarousel()}
       {renderNext()}
     </>
