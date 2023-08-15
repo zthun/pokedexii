543715cac51da7af28d9d28e901db5562ce0ba14
Anthony Bonta <zthun.coding@gmail.com>
2023-08-14 23:58:30 -0500

    build: allow for any host to listen for container support

--- packages/pokedex-web/vite.config.ts
+++ packages/pokedex-web/vite.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsConfigPaths()]
+  plugins: [tsConfigPaths()],
+  server: {
+    host: '0.0.0.0'
+  }
 });
