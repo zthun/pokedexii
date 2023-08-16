57c9e7d5d05f70e8f3075ce27d58062da000dd10
Anthony Bonta <zthun.coding@gmail.com>
2023-08-16 12:12:32 -0500

    chore: seed initial data on startup

--- packages/pokedex-api/src/app/pokedex-module.ts
+++ packages/pokedex-api/src/app/pokedex-module.ts
@@ -1,8 +1,22 @@
 /* istanbul ignore file -- @preserve */
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
+import { ZPokedexCollection } from 'src/database/pokedex-database';
+import { ZPokedexResourceModule } from '../resource/resource-module';
+import { IZPokedexResourceService, ZPokedexResourceServiceToken } from '../resource/resource-service';
 import { ZPokedexSpeciesModule } from '../species/species-module';
 
 @Module({
-  imports: [ZPokedexSpeciesModule]
+  imports: [ZPokedexSpeciesModule, ZPokedexResourceModule]
 })
-export class ZPokedexModule {}
+export class ZPokedexModule {
+  public constructor(@Inject(ZPokedexResourceServiceToken) private _resource: IZPokedexResourceService) {}
+
+  public async onModuleInit() {
+    const values = Object.values(ZPokedexCollection);
+
+    for (let i = 0; i < values.length; ++i) {
+      const resource = values[i];
+      await this._resource.populate(resource);
+    }
+  }
+}
