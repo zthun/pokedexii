0232bcca6e3c833006aa7f8f2830103e305fe09e
Anthony Bonta <zthun.coding@gmail.com>
2023-08-17 00:32:25 -0500

    feat: pokedex-api pokemon service retrieves pokemon variants

--- packages/pokedex-api/src/pokemon/pokemon.ts
+++ packages/pokedex-api/src/pokemon/pokemon.ts
@@ -1,4 +1,6 @@
-import { IPokeApiResource } from '../resource/resource';
+import { IZPokemon, ZPokemonBuilder } from '@zthun/pokedex';
+import { ZPokedexCollection } from 'src/database/pokedex-database';
+import { IPokeApiResource, ZPokeApiResource } from '../resource/resource';
 
 interface IPokeApiPokemonAbility {
   is_hidden: boolean;
@@ -80,4 +82,83 @@ export interface IPokeApiPokemon {
   stats: IPokeApiPokemonStats[];
   types: IPokeApiPokemonTypes[];
   weight: number;
+}
+
+export class ZPokeApiPokemonBuilder {
+  private _pokemon: IPokeApiPokemon;
+
+  public constructor() {
+    this.from(new ZPokemonBuilder().build());
+  }
+
+  public from(other: IZPokemon) {
+    this._pokemon = {
+      abilities: other.abilities.map((a, i) => ({
+        ability: ZPokeApiResource.toResource(ZPokedexCollection.Pokemon, a.name, a.name),
+        is_hidden: a.hidden,
+        slot: i
+      })),
+      base_experience: 1,
+      forms: [],
+      game_indices: [],
+      height: other.height,
+      held_items: [],
+      id: other.id,
+      is_default: true,
+      location_area_encounters: '',
+      moves: [],
+      name: other.name,
+      order: 1,
+      species: ZPokeApiResource.toResource(ZPokedexCollection.PokemonSpecies, other.name),
+      sprites: {
+        other: {
+          'official-artwork': {
+            front_default: other.artwork
+          }
+        }
+      },
+      stats: [
+        {
+          stat: ZPokeApiResource.toResource(ZPokedexCollection.Stat, 'hp'),
+          base_stat: other.stats.hp.base,
+          effort: other.stats.hp.effort
+        },
+        {
+          stat: ZPokeApiResource.toResource(ZPokedexCollection.Stat, 'attack'),
+          base_stat: other.stats.attack.base,
+          effort: other.stats.attack.effort
+        },
+        {
+          stat: ZPokeApiResource.toResource(ZPokedexCollection.Stat, 'defense'),
+          base_stat: other.stats.defense.base,
+          effort: other.stats.defense.effort
+        },
+        {
+          stat: ZPokeApiResource.toResource(ZPokedexCollection.Stat, 'special-attack'),
+          base_stat: other.stats.specialAttack.base,
+          effort: other.stats.specialAttack.effort
+        },
+        {
+          stat: ZPokeApiResource.toResource(ZPokedexCollection.Stat, 'special-defense'),
+          base_stat: other.stats.specialDefense.base,
+          effort: other.stats.specialDefense.effort
+        },
+        {
+          stat: ZPokeApiResource.toResource(ZPokedexCollection.Stat, 'speed'),
+          base_stat: other.stats.speed.base,
+          effort: other.stats.speed.effort
+        }
+      ],
+      types: (other.types || []).map((t, i) => ({
+        slot: i,
+        type: ZPokeApiResource.toResource(ZPokedexCollection.Type, t)
+      })),
+      weight: other.weight
+    };
+    return this;
+  }
+
+  public build() {
+    return { ...this._pokemon };
+  }
 }
