acb6516d2985f4d4c2719032fbda049f92059937
Anthony Bonta <zthun.coding@gmail.com>
2023-08-17 17:33:23 -0500

    chore: add pokedex module to app

--- packages/pokedex-api/src/app/pokedex-module.ts
+++ packages/pokedex-api/src/app/pokedex-module.ts
@@ -1,11 +1,12 @@
 /* istanbul ignore file -- @preserve */
 import { Inject, Module } from '@nestjs/common';
+import { ZPokedexPokemonModule } from '../pokemon/pokemon-module';
 import { ZPokedexResourceModule } from '../resource/resource-module';
 import { IZPokedexResourceService, ZPokedexResourceServiceToken } from '../resource/resource-service';
 import { ZPokedexSpeciesModule } from '../species/species-module';
 
 @Module({
-  imports: [ZPokedexSpeciesModule, ZPokedexResourceModule]
+  imports: [ZPokedexSpeciesModule, ZPokedexPokemonModule, ZPokedexResourceModule]
 })
 export class ZPokedexModule {
   public constructor(@Inject(ZPokedexResourceServiceToken) private _resource: IZPokedexResourceService) {}
