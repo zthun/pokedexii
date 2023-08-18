8140ca49062e521ef56c987a3d1dd4b197b19680
Anthony Bonta <zthun.coding@gmail.com>
2023-08-17 23:00:58 -0500

    code: add types module

--- packages/pokedex-api/src/app/pokedex-module.ts
+++ packages/pokedex-api/src/app/pokedex-module.ts
@@ -4,9 +4,10 @@ import { ZPokedexPokemonModule } from '../pokemon/pokemon-module';
 import { ZResourceModule } from '../resource/resource-module';
 import { IZResourceSeedService, ZResourceSeedServiceToken } from '../resource/resource-seed-service';
 import { ZSpeciesModule } from '../species/species-module';
+import { ZTypesModule } from '../type/types-module';
 
 @Module({
-  imports: [ZSpeciesModule, ZPokedexPokemonModule, ZResourceModule]
+  imports: [ZSpeciesModule, ZPokedexPokemonModule, ZResourceModule, ZTypesModule]
 })
 export class ZPokedexModule {
   public constructor(@Inject(ZResourceSeedServiceToken) private _resource: IZResourceSeedService) {}
