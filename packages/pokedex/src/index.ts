c357bfcfe1af34814d05dfd0e571fccadbc1a046
Anthony Bonta <zthun.coding@gmail.com>
2023-08-17 23:09:02 -0500

    refactor!: type service moved to api

--- packages/pokedex/src/index.ts
+++ packages/pokedex/src/index.ts
@@ -8,4 +8,3 @@ export * from './resource/resource';
 export * from './resource/resource-service';
 export * from './species/species';
 export * from './type/type';
-export * from './type/type-service';
