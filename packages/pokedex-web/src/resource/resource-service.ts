470718e54386b40674c8e467192d8049a5f1b0ed
Anthony Bonta <zthun.coding@gmail.com>
2023-08-19 18:20:13 -0500

    refactor: cleanup

--- packages/pokedex-web/src/resource/resource-service.ts
+++ packages/pokedex-web/src/resource/resource-service.ts
@@ -17,24 +17,25 @@ export class ZResourceService<T extends IZResource> implements IZResourceService
     return new ZUrlBuilder().api(location).append(this._name);
   }
 
+  public request(url: string) {
+    return new ZHttpRequestBuilder().url(url).get().timeout(5000);
+  }
+
   public async count(): Promise<number> {
     const url = this.api().page(1).size(1).build();
-    const request = new ZHttpRequestBuilder().url(url).get().build();
-    const { data: page } = await this._http.request<IZPage<T>>(request);
+    const { data: page } = await this._http.request<IZPage<T>>(this.request(url).build());
     return page.count;
   }
 
   public async retrieve(req: IZDataRequest): Promise<T[]> {
     const url = this.api().page(req.page).size(req.size).search(req.search).build();
-    const request = new ZHttpRequestBuilder().url(url).get().build();
-    const { data: page } = await this._http.request<IZPage<T>>(request);
+    const { data: page } = await this._http.request<IZPage<T>>(this.request(url).build());
     return page.data;
   }
 
   public async get(identification: number | string): Promise<T> {
     const url = this.api().append(String(identification)).build();
-    const request = new ZHttpRequestBuilder().url(url).get().timeout(30000).build();
-    const { data } = await this._http.request<T>(request);
+    const { data } = await this._http.request<T>(this.request(url).build());
     return data;
   }
 }
