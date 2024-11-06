023b37f861380b268f8c36241e019eb0b4dc9c16
Anthony Bonta <zthun.coding@gmail.com>
2024-11-05 23:47:19 -0600

    test: remove cucumber

--- cucumber.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  default: {
-    requireModule: ['ts-node/register', 'tsconfig-paths/register'],
-    require: ['packages/**/features/**/*.ts'],
-    paths: ['packages/**/features/**/*.feature'],
-    publishQuiet: true
-  }
-};
