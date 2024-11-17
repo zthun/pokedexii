40fe89ed9eb27112a43cb367cdf2bc7ee71bc54e
Anthony Bonta <zthun.coding@gmail.com>
2024-11-17 14:12:23 -0600

    refactor: esm

--- .config/helpful-proxyrc.js
+++ .config/helpful-proxyrc.js
@@ -1,17 +1,17 @@
-module.exports = {
+export default {
   domains: [
     {
-      name: 'pokedexii.local.zthunworks.com',
+      name: "pokedexii.local.zthunworks.com",
       paths: {
-        '/': 'pokedex-services-web:5173',
-        '/api': 'pokedex-services-api:3000/api'
-      }
+        "/": "pokedex-services-web:5173",
+        "/api": "pokedex-services-api:3000/api",
+      },
     },
     {
-      name: 'database.pokedexii.local.zthunworks.com',
+      name: "database.pokedexii.local.zthunworks.com",
       paths: {
-        '/': 'pokedex-mongo-express:8081'
-      }
-    }
-  ]
+        "/": "pokedex-mongo-express:8081",
+      },
+    },
+  ],
 };
