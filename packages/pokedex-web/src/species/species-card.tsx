98187b2946d97a80eb4b858810641da1b609e56d
Anthony Bonta <zthun.coding@gmail.com>
2025-01-02 15:51:48 -0600

    chore: cleanup

--- packages/pokedex-web/src/species/species-card.tsx
+++ packages/pokedex-web/src/species/species-card.tsx
@@ -1,4 +1,10 @@
-import { ZBox, ZCard, ZImageSource, ZStack } from "@zthun/fashion-boutique";
+import {
+  ZButton,
+  ZCard,
+  ZIconFontAwesome,
+  ZImageSource,
+  ZStack,
+} from "@zthun/fashion-boutique";
 import { ZSizeFixed } from "@zthun/fashion-tailor";
 import { cssJoinDefined, ZOrientation } from "@zthun/helpful-fn";
 import { IZSpecies } from "@zthun/pokedex";
@@ -18,38 +24,8 @@ export interface IZSpeciesCard {
   /**
    * The optional handler for when the card is clicked.
    */
-  onClick?: MouseEventHandler<HTMLDivElement>;
+  onClick?: MouseEventHandler<HTMLElement>;
 }
-
-/*
-const useSpeciesCardStyles = createStyleHook(({ theme, tailor }) => ({
-  root: {
-    borderRadius: "0.5rem",
-    backgroundColor: theme.surface.main,
-    color: theme.surface.contrast,
-    cursor: "pointer",
-    display: "flex",
-    flexDirection: "column",
-    padding: tailor.gap(ZSizeFixed.Medium),
-
-    "&:hover": {
-      outline: `${tailor.thickness()} solid ${theme.primary.main}`,
-    },
-  },
-
-  media: {
-    display: "flex",
-    justifyContent: "center",
-    padding: tailor.gap(ZSizeFixed.Small),
-    backgroundColor: theme.component.main,
-  },
-
-  title: {
-    fontSize: "1.2rem",
-    marginTop: tailor.gap(),
-  },
-}));
-*/
 
 /**
  * A component that displays quick species information.
@@ -74,18 +50,23 @@ export function ZSpeciesCard(props: IZSpeciesCard) {
   };
 
   return (
-    <ZBox onClick={onClick} cursor="pointer">
-      <ZCard
-        className={cssJoinDefined("ZSpeciesCard-root")}
-        data-name={species.name}
-        TitleProps={{
-          heading: startCase(species.name),
-          subHeading: `#${padStart(String(species.id), 4, "0")}`,
-        }}
-        footer={<ZTypeBadges types={species.types} />}
-      >
-        {renderContent()}
-      </ZCard>
-    </ZBox>
+    <ZCard
+      className={cssJoinDefined("ZSpeciesCard-root")}
+      data-name={species.name}
+      TitleProps={{
+        suffix: (
+          <ZButton
+            name="view"
+            label={<ZIconFontAwesome name="magnifying-glass" />}
+            onClick={onClick}
+          />
+        ),
+        heading: startCase(species.name),
+        subHeading: `#${padStart(String(species.id), 4, "0")}`,
+      }}
+      footer={<ZTypeBadges types={species.types} />}
+    >
+      {renderContent()}
+    </ZCard>
   );
 }
