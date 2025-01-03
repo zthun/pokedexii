e490a81e91ffc15982b2e51a4e78b483f5e6b5dc
Anthony Bonta <zthun.coding@gmail.com>
2025-01-03 09:59:21 -0600

    build: convert to bouncer

--- .config/bouncerrc.mjs
+++ .config/bouncerrc.mjs
@@ -1,14 +1,14 @@
 export default {
   domains: [
     {
-      name: "pokedexii.local.zthunworks.com",
+      host: "pokedexii.local.zthunworks.com",
       paths: {
         "/": "pokedex-services-web:5173",
         "/api": "pokedex-services-api:3000/api",
       },
     },
     {
-      name: "database.local.zthunworks.com",
+      host: "database.local.zthunworks.com",
       paths: {
         "/": "pokedex-mongo-express:8081",
       },
